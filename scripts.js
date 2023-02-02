a25c02b95be456588dac86cf7de1c9c0e0031c25
Sam Gillies <sam@samgillies.com>
2023-02-02 19:20:52 +0000

    fixed submit button so it adds form data to array

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let myLibrary = []; 
+let bookshelf = document.getElementById('bookshelf'); 
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -17,22 +18,28 @@ Book.prototype.info = function () {
     }
 }
 
-function addBookToLibrary(event) {
-    event.preventDefault();
+function addBookToLibrary(titleVal, authorVal, pageVal, readVal) {
+    myLibrary.push(new Book(titleVal, authorVal, pageVal, readVal));    
+}
+
+function addCard() {
+    bookshelf.createElement("div").classlist.add("library--card"); 
+}
+
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
     let titleVal = document.getElementById('title').value; 
     let authorVal = document.getElementById('author').value; 
     let pageVal = document.getElementById('pages').value; 
     let readVal = document.getElementById('read-status').value; 
 
-    myLibrary.push(new Book(titleVal, authorVal, pageVal, readVal));    
-}
+    addBookToLibrary(titleVal, authorVal, pageVal, readVal);
 
-for (let i = 0; i < myLibrary.length; i++) {
-    document.getElementById(`library--title[${i}]`).innerHTML = myLibrary[i].title; 
-    document.getElementById(`library--author[${i}]`).innerHTML = myLibrary[i].author; 
-    document.getElementById(`library--pages[${i}]`).innerHTML = myLibrary[i].pages; 
-    document.getElementById(`library--read-status[${i}]`).innerHTML = myLibrary[i].read; 
-}
+    for (let i = 0; i < myLibrary.length; i++) {
+        document.getElementById(`library--title[${i}]`).innerHTML = myLibrary[i].title; 
+        document.getElementById(`library--author[${i}]`).innerHTML = myLibrary[i].author; 
+        document.getElementById(`library--pages[${i}]`).innerHTML = myLibrary[i].pages; 
+        document.getElementById(`library--read-status[${i}]`).innerHTML = myLibrary[i].read; 
+    };  
+}); 
 
-document.getElementById('submit-button').addEventListener("click", addBookToLibrary());
-console.log(myLibrary); 
